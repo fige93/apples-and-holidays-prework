0048351fce6bf9546cd214c0a8920c1503137b73
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-24 21:50:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, day|
     day.each do |day, utility|
       binding.pry
-      if day.values.flatten(1).include?("BBQ")
+      if utility.include?("BBQ")
         day
       end
     end
